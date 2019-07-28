71f4098fd4dd7b5cd833f410e89a293b60c66f1e
erin johnson <erin.nicole.johnson@gmail.com>
2019-07-28 12:31:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index.between?(0, 8)
     true
   elsif index.between?(0, 8) && position_taken?(board, index) == false
-    false
+    true
   else
     false
   end
